ab1a22a4a2d7297d32b2765e4c9efb6f5cd13897
mee <panuphan@mycostech.com>
2018-09-12 16:06:35 +0700

    Create mean course with angular

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostService } from './../post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit, OnDestroy {
+
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post content!'},
+  //   {title: 'Second Post', content: 'This is the Second post content!'},
+  //   {title: 'Third Post', content: 'This is the Thrid post content!'}
+  // ];
+
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postService: PostService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postService.getPosts();
+    this.postsSub = this.postService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+
+}
